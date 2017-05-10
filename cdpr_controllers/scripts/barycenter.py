d3d663ee080bfe75c162f17f1f742a52334d3b3f
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2017-05-10 20:40:56 +0200

    barycenter test in Python

--- cdpr_controllers/scripts/barycenter.py
+++ cdpr_controllers/scripts/barycenter.py
@@ -3,7 +3,9 @@
 '''
 This node subscribes to a 32-length array and plots the corresponding polygon and barycenter
 '''
-
+import pylab as pl
+import roslib, rospy, rospkg
+from std_msgs.msg import Float32MultiArray
 
 
 
@@ -19,12 +21,16 @@ class Listener:
         rospy.Subscriber('barycenter', Float32MultiArray, self.read_hab)
         
     def read_hab(self, msg):
-        1
-        
-        
-        
-        
-        
+        if len(msg.data) != 32:
+            print('Message has ', msg.data, ' components')
+        else:
+            self.msg_ok = True
+            
+            for i in xrange(8):
+                self.H[i,0] = msg.data[4*i]
+                self.H[i,1] = msg.data[4*i+1]
+                self.A[i] = msg.data[4*i+2]
+                self.B[i] = msg.data[4*i+3]
         
         
 if __name__ == '__main__':
@@ -33,6 +39,102 @@ if __name__ == '__main__':
     '''    
     
     rospy.init_node('barycenter')
-
+    
+    listener = Listener()
+    
+    
+    pl.ion()
+    pl.close('all')
+    
+    F = pl.figure()
+    ax = pl.gca()    
+    
+    vertices_sorted, = pl.plot([],[], 'g', linewidth=2)
+    # barycenter
+    bary, = pl.plot([],[], 'gD', linewidth=1)
+    
+    
+    while not rospy.is_shutdown():
+        
+        if listener.msg_ok:
+            
+            H = listener.H.copy()
+            A = listener.A.copy()
+            B = listener.B.copy()
+            
+            # get vertices
+            vert = []
+            for i in xrange(8):
+                for j in xrange(8):
+                    if i != j:
+                        for u in [A[i],B[i]]:
+                            for v in [A[j],B[j]]:
+                                # intersection of Ai = Hi.x and Bj = Hj.x
+                                x = pl.dot(pl.inv(H[[i,j],:]), pl.array([u,v]))
+                                
+                                # check constraints: A <= H.x <= B
+                                if pl.amin(pl.dot(H,x) - A) >= -1e-6 and pl.amax(pl.dot(H,x) - B) <= 1e-6:                                               
+                                    vert.append(x.reshape(2).copy())            
+                            
+            # continue only if enough vertices
+            if len(vert) > 2:
+                ax.clear()
+                                
+                vert_uns = pl.array(vert + [vert[0]])
+                
+                xm,xM,ym,yM = pl.amin(vert_uns[:,0]),pl.amax(vert_uns[:,0]),pl.amin(vert_uns[:,1]),pl.amax(vert_uns[:,1])
+                ax.set_xlim(xm - 0.05*(xM-xm), xM+0.05*(xM-xm))
+                ax.set_ylim(ym - 0.05*(yM-ym), yM+0.05*(yM-ym))
+                
+                # plot lines
+                xl = pl.array([xm - 0.05*(xM-xm), xM+0.05*(xM-xm)])
+                for i in xrange(8):
+                    s = - 0.05*(yM-ym) * pl.sqrt(H[i,0]**2+H[i,1]**2)/H[i,1]
+                    # A = H.x
+                    ya = pl.array([1./H[i,1]*(A[i,0]-H[i,0]*x) for x in xl])
+                    pl.plot(xl,ya, 'b', linewidth=1)
+                    ax.fill_between(xl, ya, ya+s, facecolor='red', alpha=0.5, interpolate=True)
+                    # B = H.x
+                    ya = pl.array([1./H[i,1]*(B[i,0]-H[i,0]*x) for x in xl])
+                    pl.plot(xl,ya, 'b', linewidth=1)
+                    ax.fill_between(xl, ya, ya-s, facecolor='red', alpha=0.5, interpolate=True)                    
+                    
+                # plot unsorted vertices
+                
+                pl.plot(vert_uns[:,0], vert_uns[:,1], 'r--D', linewidth=1)
+                    
+                # sort vertices counter-clockwise
+                mid = pl.mean(vert,0)
+                vert.sort(key=lambda p: pl.arctan2(p[1]-mid[1],p[0]-mid[0]))
+                vert = pl.array(vert + [vert[0]])
+                pl.plot(vert[:,0],vert[:,1],'g',linewidth=2)
+                
+                # get center of gravity
+                a = 0
+                x = 0
+                y = 0
+                for i in xrange(vert.shape[0]):
+                    v = vert[i-1,0]*vert[i,1] - vert[i,0]*vert[i-1,1]
+                    a += v
+                    x += v*(vert[i,0]+vert[i-1,0])
+                    y += v*(vert[i,1]+vert[i-1,1])                
+                a *= 0.5
+                x /= 6*a
+                y /= 6*a
+                pl.plot([x],[y],'gD',linewidth=2)
+                
+                
+                
+                                
+                
+                pl.draw()
+                pl.pause(.0001)
+            else:
+                print('Only %i vertices compatible with constraints' % len(vert))
+    
+    
+    
+    rospy.sleep(1)
+    
 
     
--- cdpr_controllers/scripts/publish_H.py
+++ cdpr_controllers/scripts/publish_H.py
@@ -2,7 +2,34 @@
 
 import pylab as pl
 import roslib, rospy, rospkg
-from matplotlib import rc, animation
-from mpl_toolkits.axes_grid1 import host_subplot
 from std_msgs.msg import Float32MultiArray
 
+
+if __name__ == '__main__':
+    '''
+    Begin of main code
+    '''    
+    
+    rospy.init_node('dummy_HAB')
+    
+    
+    msg = Float32MultiArray()
+    msg.data = [0] * 32
+    pub = rospy.Publisher('barycenter', Float32MultiArray, queue_size=1)
+    
+    while not rospy.is_shutdown():
+        
+        for i in xrange(8):
+            # random H
+            msg.data[4*i] = pl.randn()
+            msg.data[4*i+1] = pl.randn()
+                    
+            # random A
+            msg.data[4*i+2] = -1000 - 1000*pl.rand()   
+        
+            # random B higher than A
+            msg.data[4*i+3] = msg.data[4*i+2] + 2000 + 1000*pl.rand()
+            
+        pub.publish(msg)       
+        
+        rospy.sleep(1)
